30f7d27b43315d6c09f180777b8faf20763ef4a5
manpreet <manpreet.dhaliwal22@gmail.com>
2021-02-01 09:57:17 -0800

    seed file generated for 100 ideas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Idea.delete_all
+100.times do
+    created_at=Faker::Date.backward(days: 365*5)
+   i=Idea.create(
+       title: Faker::Hacker.say_something_smart,
+       description: Faker::ChuckNorris.fact,
+       
+       created_at: created_at,
+       updated_at: created_at)
+end
+idea=Idea.all
+puts Cowsay.say("Generated #{idea.count} ideas.",:frogs)
